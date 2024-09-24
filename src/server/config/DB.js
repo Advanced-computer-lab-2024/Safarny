d0f0e1cc09ac120cb6a6c547848dac5eeb776aae
Youssef Maged <youssef.maged.6000@gmail.com>
2024-09-24 15:27:55 +0300

    update

---
+++ src/server/config/DB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in the environment variables");
+}
+
+const db = async () => {
+    try {
+        const conn = await mongoose.connect(MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default db;
