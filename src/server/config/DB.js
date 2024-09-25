7da36afe7209b86cd502ba6d5e8416dc35960469
MostafaVT <mostafaahmed96320@gmail.com>
2024-09-26 00:47:32 +0300

    FIX .env bug
    the file must in the same directory
    FIX: {path: '../../.env'}

--- src/server/config/DB.js
+++ src/server/config/DB.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({path: '../../.env'});
 
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
-    throw new Error("MONGO_URI is not defined in the environment variables");
+    throw new Error("MONGO_URI is not defined in the environment variables ");
 }
 
 const db = async () => {
--- src/server/main.js
+++ src/server/main.js
@@ -1,25 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import ViteExpress from "vite-express";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import User from './models/userModel.js';// Import the default export (User model)
-import DB from './config/DB.js';
-import signUp from './routes/signUpRoutes.js';
-import Users from './routes/usersRoutes.js';
-import login from './routes/loginRoutes.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import User from "./models/userModel.js"; // Import the default export (User model)
+import DB from "./config/DB.js";
+import signUp from "./routes/signUpRoutes.js";
+import Users from "./routes/usersRoutes.js";
+import login from "./routes/loginRoutes.js";
 dotenv.config();
-
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-const port = process.env.PORT || 3000;
-
-
-
+const port = process.env.PORT;
 DB();
 
 /*app.post('/signup', async (req, res) => {
@@ -117,11 +113,11 @@ app.post('/login', async (req, res) => {
 });
 */
 
-app.use('/signup',signUp)
-app.use('/signup/addadmin',signUp)
-app.use('/users',Users)
-app.use('/login',login)
+app.use("/signup", signUp);
+app.use("/signup/addadmin", signUp);
+app.use("/users", Users);
+app.use("/login", login);
 
 ViteExpress.listen(app, 3000, () => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
