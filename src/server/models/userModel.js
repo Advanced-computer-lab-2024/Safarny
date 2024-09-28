7dabb67cd7a4c894f49043e74555cd1c5fc1820e
MostafaVT <mostafaahmed96320@gmail.com>
2024-09-29 00:25:00 +0300

    Refactor user model to separate fields based on user type

--- src/server/models/userModel.js
+++ src/server/models/userModel.js
@@ -2,22 +2,40 @@ const mongoose = require("mongoose");
 
 const userModel = new mongoose.Schema(
   {
+    //Default
     username: { type: String, required: true, unique: true },
     email: String,
     password: String,
-    nationality: String,
-    mobile: String,
-    employed: String,
-    type: String,
-    age : Number,
-      YearOfExp:Number,
-      PrevWork:String,
 
+    //Role
+    type: String,
+
+    //Tourist
+    nationality: String,
+    employed: String,
+    wallet: Number,
+    DOB: Date,
+    age: Number,
+    mobile: String,
+
+    //TourGuide
+    YearOfExp: Number,
+    PrevWork: String,
+
+    //Seller
+    description: String,
+    fname: String,
+    lname: String,
+
+    //Advertiser
+    CompanyName: String,
+    CompanyLink: String,
+    CompanyHotline: Number,
   },
   {
-    collection: "datainfo",
+    collection: "User",
   }
 );
 
-const User = mongoose.model("datainfo", userModel);
+const User = mongoose.model("User", userModel);
 module.exports = User;
