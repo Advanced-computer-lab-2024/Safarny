b1e73aa42c1604e589a3b1a8a4f0afbb985a8154
Tarek Sherif <tareksherif2020@gmail.com>
2024-11-09 23:04:23 +0200

    view seller name in productlist (wip)

--- src/client/Components/ProductView/ProductList.jsx
+++ src/client/Components/ProductView/ProductList.jsx
@@ -12,6 +12,7 @@ const ProductList = () => {
   const { userId: initialUserId } = location.state || {};
 
   const [userId, setUserId] = useState(initialUserId || localStorage.getItem('userId'));
+  const [sellers, setSellers] = useState({});
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -53,6 +54,23 @@ const ProductList = () => {
 
         if (Array.isArray(response.data)) {
           setProducts(response.data);
+
+          // Fetch the names of the sellers based on their ObjectId
+          const sellerIds = response.data.map(product => product.createdby);
+          const uniqueSellerIds = [...new Set(sellerIds)];
+
+          const sellerPromises = uniqueSellerIds.map(id =>
+            axios.get(`/user/${id}`).then(res => res.data)
+          );
+
+          const sellerData = await Promise.all(sellerPromises);
+
+          const sellersMap = sellerData.reduce((acc, seller) => {
+            acc[seller._id] = seller.name; // Assuming the seller's name is stored in `name` field
+            return acc;
+          }, {});
+
+          setSellers(sellersMap);
         } else {
           console.error("Expected an array, but got:", typeof response.data);
           setError('Invalid data format received');
@@ -65,6 +83,7 @@ const ProductList = () => {
         setLoading(false);
       }
     };
+
 
     const fetchUserRole = async () => {
       try {
@@ -85,7 +104,7 @@ const ProductList = () => {
     fetchExchangeRates();
   }, [userId]);
 
-  
+
 
   const handleAddToWishlist = async (postId) => {
     try {
@@ -111,7 +130,7 @@ const ProductList = () => {
 
     try {
       console.log("userid: ", userId);
-      const updatedProduct = {...product, quantity: product.quantity - 1, purchasedCount: product.purchasedCount + 1};
+      const updatedProduct = { ...product, quantity: product.quantity - 1, purchasedCount: product.purchasedCount + 1 };
       await axios.put(`/admin/products/${product._id}`, updatedProduct);
       await axios.put(`/admin/products/${product._id}`, updatedProduct);
       const profileResponse = await axios.get(`http://localhost:3000/tourist/${userId}`);
@@ -166,115 +185,117 @@ const ProductList = () => {
   }
 
   return (
-      <div className={styles.container}>
-        <Header/>
+    <div className={styles.container}>
+      <Header />
 
-        <button
-            className={styles.viewPurchasedButton}
-            onClick={handleClick}
-        >
-          View Purchased Products
-        </button>
-        <h1 className={styles.h1}>Product List</h1>
-        <input
-            type="text"
-            placeholder="Search by name..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className={styles.searchInput}
-        />
+      <button
+        className={styles.viewPurchasedButton}
+        onClick={handleClick}
+      >
+        View Purchased Products
+      </button>
+      <h1 className={styles.h1}>Product List</h1>
+      <input
+        type="text"
+        placeholder="Search by name..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className={styles.searchInput}
+      />
 
-        <div className={styles.filterContainer}>
-          <div className={styles.sortOptions}>
-            <label className={styles.MaxPrice}>Max Price:</label>
-            <input
-                type="number"
-                value={priceFilter}
-                onChange={(e) => setPriceFilter(e.target.value)}
-                className={styles.priceInput}
-            />
-          </div>
-          <div className={styles.currencySelector}>
-            <FormControl fullWidth margin="normal">
-              <InputLabel><h4>Currency</h4></InputLabel>
-              <br></br>
-              <Select
-                  value={selectedCurrency}
-                  onChange={(e) => setSelectedCurrency(e.target.value)}
-              >
-                {currencyCodes.map(code => (
-                    <MenuItem key={code} value={code}>{code}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </div>
-          <div className={styles.sortOptions}>
-            <label className={styles.sorter}>
-              Sort by Ratings:
-              <input
-                  type="checkbox"
-                  checked={sortByRating}
-                  onChange={() => setSortByRating(!sortByRating)}
-                  className={styles.largeCheckbox}
-              />
-            </label>
-          </div>
+      <div className={styles.filterContainer}>
+        <div className={styles.sortOptions}>
+          <label className={styles.MaxPrice}>Max Price:</label>
+          <input
+            type="number"
+            value={priceFilter}
+            onChange={(e) => setPriceFilter(e.target.value)}
+            className={styles.priceInput}
+          />
         </div>
-
-        {sortedProducts.length > 0 ? (
-            <div className={styles.productsAll}>
-              {sortedProducts.map(product => {
-                const convertedPrice = convertPrice(product.price, product.currency, selectedCurrency);
-                console.log("Product Reviews:", product.reviews); // Check the structure of reviews
-                return (
-                    <div className={styles.productCard} key={product._id}>
-                      <h2 className={styles.productDetails}>{product.details}</h2>
-                      <p>Price: {convertedPrice} {selectedCurrency}</p>
-                      <p>Quantity: {product.quantity}</p>
-                      <p>Rating: {product.rating}</p>
-                      {userRole === 'Seller' && (
-                          <>
-                            <p>Purchased Count: {product.purchasedCount}</p>
-                            <p>Sales: {product.purchasedCount * product.price}</p>
-                          </>
-                      )}
-                      {/* Display Reviews */}
-                      <div className={styles.reviewsSection}>
-                        <h3>Reviews:</h3>
-                        {product.reviews && product.reviews.length > 0 ? (
-                            <ul>
-                              {product.reviews.map((review, index) => (
-                                  <li key={index}>{review}</li>
-                              ))}
-                            </ul>
-                        ) : (
-                            <p>No reviews available</p>
-                        )}
-                      </div>
-                      <button
-                          className={styles.buyButton}
-                          onClick={() => handleBuyButtonClick(product)}
-                      >
-                        Purchase
-                      </button>
-                      {userRole === 'Tourist' && (
-                          <button
-                              onClick={() => handleAddToWishlist(product._id)}
-                              className={styles.wishlistButton}
-                          >
-                            Add to Wishlist
-                          </button>
-                      )}
-                      <img className={styles.productImage} src={product.imageurl} alt={product.details}/>
-                    </div>
-                );
-              })}
-            </div>
-        ) : (
-            <p>No products available</p>
-        )}
-        <Footer/>
+        <div className={styles.currencySelector}>
+          <FormControl fullWidth margin="normal">
+            <InputLabel><h4>Currency</h4></InputLabel>
+            <br></br>
+            <Select
+              value={selectedCurrency}
+              onChange={(e) => setSelectedCurrency(e.target.value)}
+            >
+              {currencyCodes.map(code => (
+                <MenuItem key={code} value={code}>{code}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className={styles.sortOptions}>
+          <label className={styles.sorter}>
+            Sort by Ratings:
+            <input
+              type="checkbox"
+              checked={sortByRating}
+              onChange={() => setSortByRating(!sortByRating)}
+              className={styles.largeCheckbox}
+            />
+          </label>
+        </div>
       </div>
+
+      {sortedProducts.length > 0 ? (
+        <div className={styles.productsAll}>
+          {sortedProducts.map(product => {
+            const convertedPrice = convertPrice(product.price, product.currency, selectedCurrency);
+            const sellerName = sellers[product.createdby] || "Unknown Seller";
+            console.log("Product Reviews:", product.reviews); // Check the structure of reviews
+            return (
+              <div className={styles.productCard} key={product._id}>
+                <h2 className={styles.productDetails}>{product.details}</h2>
+                <p>Price: {convertedPrice} {selectedCurrency}</p>
+                <p>Quantity: {product.quantity}</p>
+                <p>Rating: {product.rating}</p>
+                <p>Seller: {sellerName}</p>
+                {userRole === 'Seller' && (
+                  <>
+                    <p>Purchased Count: {product.purchasedCount}</p>
+                    <p>Sales: {product.purchasedCount * product.price}</p>
+                  </>
+                )}
+                {/* Display Reviews */}
+                <div className={styles.reviewsSection}>
+                  <h3>Reviews:</h3>
+                  {product.reviews && product.reviews.length > 0 ? (
+                    <ul>
+                      {product.reviews.map((review, index) => (
+                        <li key={index}>{review}</li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>No reviews available</p>
+                  )}
+                </div>
+                <button
+                  className={styles.buyButton}
+                  onClick={() => handleBuyButtonClick(product)}
+                >
+                  Purchase
+                </button>
+                {userRole === 'Tourist' && (
+                  <button
+                    onClick={() => handleAddToWishlist(product._id)}
+                    className={styles.wishlistButton}
+                  >
+                    Add to Wishlist
+                  </button>
+                )}
+                <img className={styles.productImage} src={product.imageurl} alt={product.details} />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <p>No products available</p>
+      )}
+      <Footer />
+    </div>
   );
 };
 
